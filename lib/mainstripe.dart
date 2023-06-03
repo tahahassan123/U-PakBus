5cfbe68ab4e2ac963b3fecc260d265ff09b9fe70
Muhammd Ali Masood <md.ali2001@hotmail.com>
2023-06-03 03:18:29 -0700

    Update mainstripe.dart

--- lib/mainstripe.dart
+++ lib/mainstripe.dart
@@ -1,31 +1,127 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:flutter_stripe/flutter_stripe.dart';
-import 'package:pakistanbusapp/stripe.dart';
 
 
-void main() async{
 
-  WidgetsFlutterBinding.ensureInitialized();
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
 
-  Stripe.publishableKey = 'pk_test_51NCzkJI3GjRc0k0G7UYdLn0T3O9dUl2cIHzixhgztwcPz5bfAbaqknMWa5sTfKUo9X2Hx27VpqZH7y8LZoSNtChU000A6VQLAd';
-
-  await Stripe.instance.applySettings();
-  runApp(const MyApp());
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class _HomeScreenState extends State<HomeScreen> {
 
-  // This widget is the root of your application.
+  Map<String, dynamic>? paymentIntent;
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-
-        primarySwatch: Colors.blue,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Stripe Payment'),
       ),
-      home: const HomeScreen(),
+      body: Center(
+        child: TextButton(
+          child: const Text('Make Payment'),
+          onPressed: () async {
+            await makePayment();
+          },
+        ),
+      ),
     );
   }
+
+  Future<void> makePayment() async {
+    try {
+      paymentIntent = await createPaymentIntent('10', 'USD');
+      //Payment Sheet
+      await Stripe.instance.initPaymentSheet(
+          paymentSheetParameters: SetupPaymentSheetParameters(
+              paymentIntentClientSecret: paymentIntent!['client_secret'],
+              // applePay: const PaymentSheetApplePay(merchantCountryCode: '+92',),
+              // googlePay: const PaymentSheetGooglePay(testEnv: true, currencyCode: "US", merchantCountryCode: "+92"),
+              style: ThemeMode.dark,
+              merchantDisplayName: 'Adnan')).then((value) {});
+
+
+      ///now finally display payment sheeet
+      displayPaymentSheet();
+    } catch (e, s) {
+      print('exception:$e$s');
+    }
+  }
+
+  displayPaymentSheet() async {
+    try {
+      await Stripe.instance.presentPaymentSheet(
+      ).then((value) {
+        showDialog(
+            context: context,
+            builder: (_) =>
+                AlertDialog(
+                  content: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Row(
+                        children: const [
+                          Icon(Icons.check_circle, color: Colors.green,),
+                          Text("Payment Successfull"),
+                        ],
+                      ),
+                    ],
+                  ),
+                ));
+        // ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text("paid successfully")));
+
+        paymentIntent = null;
+      }).onError((error, stackTrace) {
+        print('Error is:--->$error $stackTrace');
+      });
+    } on StripeException catch (e) {
+      print('Error is:---> $e');
+      showDialog(
+          context: context,
+          builder: (_) =>
+          const AlertDialog(
+            content: Text("Cancelled "),
+          ));
+    } catch (e) {
+      print('$e');
+    }
+  }
+
+  //  Future<Map<String, dynamic>>
+  createPaymentIntent(String amount, String currency) async {
+    try {
+      Map<String, dynamic> body = {
+        'amount': calculateAmount(amount),
+        'currency': currency,
+        'payment_method_types[]': 'card'
+      };
+
+      var response = await http.post(
+        Uri.parse('https://api.stripe.com/v1/payment_intents'),
+        headers: {
+          'Authorization': 'Bearer sk_test_51NCzkJI3GjRc0k0Gke4rcq5Aq61auu3Tel6fceIW8GdefxyMU8Wa2WgjfnfHHYbevbJ7RwG1s4d3PnrIO0FvOJVB00mdyakBmI',
+          'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: body,
+      );
+      // ignore: avoid_print
+      print('Payment Intent Body->>> ${response.body.toString()}');
+      return jsonDecode(response.body);
+    } catch (err) {
+      // ignore: avoid_print
+      print('err charging user: ${err.toString()}');
+    }
+  }
+
+  calculateAmount(String amount) {
+    final calculatedAmout = (int.parse(amount)) * 100;
+    return calculatedAmout.toString();
+  }
+
 }
